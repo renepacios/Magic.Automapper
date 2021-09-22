f1dbee62a5e2a190aa33ed9fcc146b35a9d1d9cf
René Pacios <3912951+renepacios@users.noreply.github.com>
2021-09-22 22:12:35 +0200

    Added test projects.  config yml project names.

---
+++ Magic.AutoMapper.Extensions.Microsoft.DependencyInjection.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Magic.AutoMapper.Extensions.Microsoft.DependencyInjection.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
